5e241e80abeea5717216e66cc2a6fa25c6f1d9c9
stopitcarl <joaofporto@tecnico.ulisboa.pt>
2018-10-03 15:46:37 +0100

    Some chnages

--- CircuitRouter-SeqSolver/CircuitRouter-SeqSolver.c
+++ CircuitRouter-SeqSolver/CircuitRouter-SeqSolver.c
@@ -150,9 +150,12 @@ static FILE * parseArgs(long argc, char* const argv[]) {
 	 }
 	 */
 
-	assert(argc == 2);
-	FILE * fileToRead = fopen(argv[1], 'r');
-	assert(fileToRead);
+	// Argument check
+	assert(argc == 2);	
+	// Open file
+	const char modeToRead = 'r';
+	FILE * fileToRead = fopen(argv[1], &modeToRead);
+	assert(fileToRead);	
 	return fileToRead;
 }
 
